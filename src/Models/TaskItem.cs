b5fe1d2452696ad623490c604f0ad5fa9cb92108
Apolline <apolline.fontaine@epitech.eu>
2025-11-10 15:32:46 +0100

    adding a Unique identifer to the TaskItem class

--- src/Models/TaskItem.cs
+++ src/Models/TaskItem.cs
@@ -1,7 +1,10 @@
+using System;
 namespace TodoListApp.Models;
 
 public class TaskItem
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
+
     public string Title { get; set; } = string.Empty;
     public bool IsCompleted { get; set; } = false;
 }
